f574a0692b2d9752b4aaf7058f4225fa229ebb23
Sirish22 <sirishshrestha22@gmail.com>
2023-01-02 18:58:40 +0545

    "Commit"

---
+++ ContactAPI/Controllers/ContactsController.cs
@@ -0,0 +1,50 @@
+﻿using ContactAPI.Data;
+using ContactAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext dbContext;
+
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetContacts()
+        {
+           return Ok(await dbContext.Contacts.ToListAsync());
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                Address = addContactRequest.Address,
+                Email = addContactRequest.Email,
+                FullName = addContactRequest.FullName,
+                Phone = addContactRequest.Phone
+            };
+
+            await dbContext.Contacts.AddAsync(contact);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(contact);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateContact([FromRoute] Guid id, UpdateContactRequest updateContactRequest )
+       {
+            dbContext.Contacts.Find(id)
+       }
+    }
+}
