1ff3618a3eacfbf960219867046add085852c976
Sirish22 <sirishshrestha22@gmail.com>
2023-01-03 18:21:02 +0545

    Update and delete created

--- ContactAPI/Controllers/ContactsController.cs
+++ ContactAPI/Controllers/ContactsController.cs
@@ -21,7 +21,19 @@ namespace ContactAPI.Controllers
            return Ok(await dbContext.Contacts.ToListAsync());
         }
 
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetContact([FromRoute]Guid id)
+        {
+            var contanct = await dbContext.Contacts.FindAsync(id);
+            if(contanct == null)
+            {
+                return NotFound();
+            }
+            return Ok(contanct);
+        }
 
+     
         [HttpPost]
         public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
         {
@@ -44,8 +56,34 @@ namespace ContactAPI.Controllers
         [Route("{id:guid}")]
         public async Task<IActionResult> UpdateContact([FromRoute] Guid id, UpdateContactRequest updateContactRequest )
        {
-            dbContext.Contacts.Find(id)
+            var contanct = await dbContext.Contacts.FindAsync(id);
+
+            if(contanct != null)
+            {
+                contanct.FullName = updateContactRequest.FullName;
+                contanct.Address = updateContactRequest.Address;
+                contanct.Phone = updateContactRequest.Phone;
+                contanct.Email = updateContactRequest.Email;
+
+                await dbContext.SaveChangesAsync();
+
+                return Ok(contanct);
+            }
+            return NotFound();
        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteContact([FromRoute]Guid id)
+        {
+            var  contact = await dbContext.Contacts.FindAsync(id);
+            if(contact != null)
+            {
+                dbContext.Remove(contact);
+                await dbContext.SaveChangesAsync();
+                return Ok(contact);
+            }
+            return NotFound();
+        }
     }
 }
-dasdasd
